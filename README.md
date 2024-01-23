559590d0ce5f33ede664e0e4b6b5a2005cda34fb
Keefer Taylor | Tessellated <keefer@tessellated.io>
2024-01-23 14:52:07 -0800

    Provide validator registry and All Chains Endpoint (#1)
    * support /chains
    
    * more features
    
    * fix flags in docs
    
    * mess with some more paths

--- README.md
+++ README.md
@@ -1,28 +1,64 @@
-# Planetarium
+<div align="center">
+<p style="font-size:24px; font-weight: bold;">Planetarium</p>
+<p>
+      <img alt="Tessellated Logo" src="media/tessellated-logo.png" />
+<small>Software by <a href="https://tessellated.io" target="_blank"> Tessellated // tessellated.io</a></small>
+</p>
+</div>
 
-Planetarium is a small, simple server that hosts the CosmosHub Chain Registry. 
+---
+Planetarium is a small server for the [Cosmos Chain Registry](https://github.com/cosmos/chain-registry) and [EcoStake Validator Registry](https://github.com/eco-stake/validator-registry).
 
-You can access it at [chain-registry.tessellated.io](https://chain-registry.tessellated.io)
+Planetarium is aimed to be simple, easy to deploy, and manage updates. Tessellated provides a hosted implementation of this API, which you can find at [planetarium.tessellated.io](https://planetarium.tessellated.io).
+
+## API 
+
+Planetarium has two namespaces:
+- `/v1/chains`: For information from the chain registry
+- `/v1/validators`: For information from the validator registry
+
+In most cases, the existing file structure of the validator and chain registry are provided as-is. For instance, to find [`cosmoshub/chain.json`](https://github.com/cosmos/chain-registry/blob/0995e7859e47136a7cb98d97c0e93739f268b091/cosmoshub/chain.json) you would simply `curl planetarium.tessellated.io/v1/chains/cosmoshub/chain.json`. Likewise, to find [`tessellated/profile.json`](https://github.com/eco-stake/validator-registry/blob/e1a994fdd93af96d021fe14bd52815c2adb793cf/tessellated/profile.json) from the validator registry: ``curl planetarium.tessellated.io/v1/validators/tessellated/profile.json``.
+
+These additional routes are provided for convenience:
+- **`/v1/chains/all`**: List all chains
 
 ## Installing
 
+It's easy to install or self host Planetarium. 
+
+### Get Planetarium
+
 ```shell
-$ git clone https://github.com/tessellated-io/planetarium # (Use --recursive if you want to pull chain-registry automatically).
+$ git clone https://github.com/tessellated-io/planetarium # (Use --recursive if you want to pull chain-registry and validator-registry which are defined as submodules).
 $ make install
  
 $ planetarium --help
 ```
 
-## Usage
+### Set Up Registry Data
 
+Rather than provide an opaque or configurable update strategy, Planetarium simply pulls static data from your disk and hosts it. At runtime, you specify the directories, and thus can configure it to update however you want (manually, via a cron job, etc).
+
+> **NOTE:** A side effect is that anything inside of your chain and validator registry folders is hosted. Be sure to not inadvertently expose sensitive data. 
+
+You'll need to clone the Chain and Validator registry:
 ```shell
-$ planetarium start --port 8080 --file-path /home/user/chain-registry
+$ git clone https://github.com/cosmos/chain-registry /home/user/chain-registry
+$ git clone https://github.com/eco-stake/validator-registry /home/user/validator-registry 
 ```
 
-## API 
+### Run Planetarium
 
-Files are exposed in the same way they are in the [Chain Registry](https://github.com/cosmos/chain-registry/). For instance, to find [`cosmoshub/chain.json`] you would simply `curl 127.0.0.1:5353/cosmoshub.chain.json`.
+```shell
+$ planetarium start --chain-registry-directory /home/user/chain-registry/ -validator-registry-directory /home/user/validator-registry/ --port 8080
+```
 
-## Updates
+### Updating Registry Data
 
-You specify the directory that contains the chain registry, and thus can configure it to update however you want (manually, via a cron job, etc)
+Simply git pull any updates, though you will likely want to run this on a cron job or similar
+
+```shell
+$ cd /home/user/chain-registry && git pull
+$ cd /home/user/validator-registry && git pull
+```
+
--- server/server.go
+++ server/server.go
@@ -1,30 +1,79 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/fs"
 	"net/http"
+	"path/filepath"
+	"strings"
 	"time"
 
+	"github.com/tessellated-io/pickaxe/arrays"
 	"github.com/tessellated-io/pickaxe/log"
 )
 
+/** Constants */
+
+const apiVersion = "v1"
+
+const (
+	chainsNamespace     = "chains"
+	validatorsNamespace = "validators"
+)
+
+const allChainsEndpoint = "all"
+
+/** Type and constructor */
+
 type Server struct {
-	logger                 *log.Logger
-	staticContentDirectory string
+	logger                     *log.Logger
+	chainRegistryDirectory     string
+	validatorRegistryDirectory string
 }
 
-func NewServer(staticContentDirectory string, logger *log.Logger) (*Server, error) {
+func NewServer(chainRegistryDirectory, validatorRegistryDirectory string, logger *log.Logger) (*Server, error) {
+	// Remove trailing / on inputs, which mess up the all chains helper
+	normalizedChainRegistryDirectory := chainRegistryDirectory
+	if strings.HasSuffix(chainRegistryDirectory, "/") {
+		normalizedChainRegistryDirectory = filepath.Dir(chainRegistryDirectory)
+	}
+	normalizedValidatorRegistryDirectory := validatorRegistryDirectory
+	if strings.HasSuffix(validatorRegistryDirectory, "/") {
+		normalizedValidatorRegistryDirectory = filepath.Dir(validatorRegistryDirectory)
+	}
+
 	return &Server{
-		staticContentDirectory: staticContentDirectory,
-		logger:                 logger,
+		chainRegistryDirectory:     normalizedChainRegistryDirectory,
+		validatorRegistryDirectory: normalizedValidatorRegistryDirectory,
+		logger:                     logger,
 	}, nil
 }
 
-func (s *Server) Start(port int) error {
-	s.logger.Debug().Str("static_content_dir", s.staticContentDirectory).Msg("starting to serve static content")
+/** Public API */
 
-	fs := http.FileServer(http.Dir(s.staticContentDirectory))
-	http.Handle("/", fs)
+func (s *Server) Start(port int) error {
+	s.logger.Debug().Str("chain_registry_directory", s.chainRegistryDirectory).Str("validator_registry_directory", s.validatorRegistryDirectory).Int("port", port).Msg("starting to serve static content")
+
+	versionedChainsNamespace := fmt.Sprintf("/%s/%s", apiVersion, chainsNamespace)
+	versionedValidatorsNamespace := fmt.Sprintf("/%s/%s", apiVersion, validatorsNamespace)
+
+	// Static content hosting for chains and validator registry
+	chainRegistryFileServer := http.FileServer(http.Dir(s.chainRegistryDirectory))
+	http.Handle(versionedChainsNamespace, http.StripPrefix(versionedChainsNamespace, chainRegistryFileServer))
+	http.Handle(fmt.Sprintf("%s/", versionedChainsNamespace), http.StripPrefix(fmt.Sprintf("%s/", versionedChainsNamespace), chainRegistryFileServer))
+	s.logger.Debug().Str("endpoint", versionedChainsNamespace).Msg("hosting chain registry")
+
+	validatorRegistryFileServer := http.FileServer(http.Dir(s.validatorRegistryDirectory))
+	http.Handle(versionedValidatorsNamespace, http.StripPrefix(versionedValidatorsNamespace, validatorRegistryFileServer))
+	http.Handle(fmt.Sprintf("%s/", versionedValidatorsNamespace), http.StripPrefix(fmt.Sprintf("%s/", versionedValidatorsNamespace), validatorRegistryFileServer))
+	s.logger.Debug().Str("endpoint", versionedValidatorsNamespace).Msg("hosting validator registry")
+
+	// Convenience endpoints
+	versionedAllChainsEndpoint := fmt.Sprintf("/%s/%s/%s", apiVersion, chainsNamespace, allChainsEndpoint)
+	http.HandleFunc(versionedAllChainsEndpoint, s.allChains)
+	http.HandleFunc(fmt.Sprintf("%s/", versionedAllChainsEndpoint), s.allChains)
+	s.logger.Debug().Str("endpoint", versionedAllChainsEndpoint).Msg("hosting all chains helper")
 
 	server := &http.Server{
 		Addr:              fmt.Sprintf(":%d", port),
@@ -38,4 +87,66 @@ func (s *Server) Start(port int) error {
 	}
 
 	return nil
+}
+
+/** Handlers */
+
+func (s *Server) allChains(w http.ResponseWriter, req *http.Request) {
+	requestMethod := "all_chains"
+	s.logger.Info().Str("method", requestMethod).Msg("💻 handling request")
+
+	// Walk through all files/folders in the root, looking for folders
+	folders := []string{}
+	err := filepath.WalkDir(s.chainRegistryDirectory, func(path string, directoryEntry fs.DirEntry, err error) error {
+		s.logger.Debug().Str("item", directoryEntry.Name()).Msg("examining item")
+
+		if err != nil {
+			s.logger.Debug().Str("item", directoryEntry.Name()).Err(err).Msg("error examining item")
+			return err
+		}
+
+		// Do not process containing directory
+		if directoryEntry.IsDir() && path == s.chainRegistryDirectory {
+			s.logger.Debug().Str("item", directoryEntry.Name()).Str("path", path).Msg("skipping processing of chain-registry root directory")
+			return nil
+		}
+
+		// Skip subdirectories
+		if directoryEntry.IsDir() && filepath.Dir(path) != s.chainRegistryDirectory {
+			s.logger.Debug().Str("item", directoryEntry.Name()).Str("path", path).Msg("skipping subdirectory")
+			return filepath.SkipDir
+		}
+
+		if directoryEntry.IsDir() {
+			s.logger.Debug().Str("item", directoryEntry.Name()).Msg("noting item as a directory")
+			folders = append(folders, directoryEntry.Name())
+		}
+		s.logger.Debug().Str("item", directoryEntry.Name()).Msg("item is not a directory")
+		return nil
+	})
+	if err != nil {
+		s.logger.Error().Err(err).Str("method", requestMethod).Msg("🚨 error traversing directories while handling request")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Filter out folders that are special or metadata
+	chains := arrays.Filter(folders, func(input string) bool {
+		isHidden := strings.HasPrefix(input, ".")
+		isMetadata := strings.HasPrefix(input, "_")
+		isTestnet := strings.EqualFold("testnets", input)
+
+		return !isHidden && !isMetadata && !isTestnet
+	})
+
+	// Serialize to json
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(chains)
+	if err != nil {
+		s.logger.Error().Err(err).Str("method", requestMethod).Msg("🚨 error serializing json while handling request")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	s.logger.Info().Str("method", requestMethod).Msg("💡 successfully handled request")
 }
